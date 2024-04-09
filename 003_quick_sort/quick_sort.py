c2be2712b8f0fb3e2971ceb8accde34efd69a1ac
salvador-blanco <dabmoc@gmail.com>
2024-04-09 11:01:38 -0600

    quick_sort.py has been updated to implement in-place sorting

--- 003_quick_sort/quick_sort.py
+++ 003_quick_sort/quick_sort.py
@@ -1,41 +1,32 @@
 import random
 
-def quick_sort(input_array, low, max):
-    print()
-    print(f"low: {low} max: {max}")
-    print(input_array)
-    print(f"input array {input_array[low:max]}")
-    array_len = len(input_array[low:max])
+def quick_sort(input_array, segment_start, segment_end):
+
+    array_len = len(input_array[segment_start:segment_end])
 
     if array_len <= 1:
-        return input_array[low:max]
+        return input_array[segment_start:segment_end]
     
-    pivot = random.randint(low, max)
-    print(f"Pivot: {input_array[pivot]}")
-    input_array[max-1], input_array[pivot] = input_array[pivot], input_array[max-1]
-    print(f"pivot moved {input_array[low:max]}")
-    pivot = max-1
-    print(f"Pivot: {input_array[pivot]}")
+    pivot = random.randint(segment_start, segment_end-1)
+    input_array[segment_end-1], input_array[pivot] = input_array[pivot], input_array[segment_end-1]
+    pivot = segment_end-1
 
 
-    i, j = 0, 0
+    partition_index = segment_start
 
-    for j in range(low,max-1):
+    for j in range(segment_start,segment_end-1):
         if input_array[j] < input_array[pivot]:
-            input_array[j], input_array[i] = input_array[i], input_array[j]
-            i+=1
-    input_array[pivot], input_array[i] = input_array[i], input_array[pivot]
-    pivot = i
+            input_array[j], input_array[partition_index] = input_array[partition_index], input_array[j]
+            partition_index+=1
+    input_array[pivot], input_array[partition_index] = input_array[partition_index], input_array[pivot]
+    pivot = partition_index
 
-    print(f"sorted array: {input_array[low:max]}")
-    print(f"next itter 1: {input_array[low:pivot]}")
-    print(f"next iiter p: {input_array[pivot]}")
-    print(f"next itter 2: {input_array[pivot+1:max]}")
-
-    return quick_sort(input_array, low, pivot)  + [input_array[pivot]] + quick_sort(input_array, pivot+1, max)
+    quick_sort(input_array, segment_start, pivot)
+    quick_sort(input_array, pivot+1, segment_end)
+    return input_array
 
 def main():
-    test_array = [50, 16, 87, 51, 79, 23]#, 61, 77, 5, 48, 40, 11, 42, 63, 7, 1, 55, 99, 64]
+    test_array = [50, 16, 87, 51, 79, 23, 61, 77, 5, 48, 40, 11, 42, 63, 7, 1, 55, 99, 64]
     
     print(f"Test array: {test_array}")
     sorted_array = quick_sort(test_array, 0, len(test_array))
