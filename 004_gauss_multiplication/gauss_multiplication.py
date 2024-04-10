0d6886899015c5658896d3cfe4a0183adb1d8a38
salvador-blanco <dabmoc@gmail.com>
2024-04-10 16:20:04 -0600

    gauss_multiplication.py has been updated

--- 004_gauss_multiplication/gauss_multiplication.py
+++ 004_gauss_multiplication/gauss_multiplication.py
@@ -1,15 +1,18 @@
+import math
+
 def gauss_multiplication(x, y):
     
     
-    len_xy = len(str(x))
+    len_xy = max(len(str(x)), len(str(y)))
+    len_xy_min = min(len(str(x)), len(str(y)))
     
-    if len_xy <=1 or len(str(y)):
+    if len(str(x)) <=1 or len(str(y)) <= 1:
         return x*y
     
     x, y = str(x), str(y)
-
-    x0, x1 = x[0:len_xy//2], x[len_xy//2:]
-    y0, y1 = y[0:len_xy//2], y[len_xy//2:]
+    
+    x0, x1 = x[0:math.ceil(len(x)/2)], x[math.ceil(len(x)/2):]
+    y0, y1 = y[0:math.ceil(len(y)/2)], y[math.ceil(len(y)/2):]
 
     x1, x0, y1, y0 = int(x0), int(x1), int(y0), int(y1)
 
@@ -22,17 +25,20 @@ def gauss_multiplication(x, y):
         v = x0*y0
         w = (x1 + x0)*(y1 + y0)
 
-    z = w - (u+v)
-    p1 =  u*(10**len_xy)
-    p2 = z*(10**(len_xy//2))
+    z = w -u -v
+    p1 =  u*(10**len_xy_min)
+    p2 = z*(10**(len_xy_min//2))
     p = +  p1 + p2 + v
 
     return p
 
+
 def main():
-    a = 121
-    b = 56
+    a = 23
+    b = 345
+
     p = gauss_multiplication(a,b) #7 006 652
     print(p)
+
 if __name__ == "__main__":
     main()
